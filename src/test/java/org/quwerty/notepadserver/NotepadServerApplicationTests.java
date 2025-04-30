1bfc6f2c734397c1f92418e033bc5c46b6315d63
QUwerty1 <mrquwerty@mail.ru>
2025-04-30 21:29:01 +0800

    init

---
+++ src/test/java/org/quwerty/notepadserver/NotepadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quwerty.notepadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
